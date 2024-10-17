5796c5b2418a7e22ff470e60890e92afffdbcd6b
lumonka <93397867+lumonka@users.noreply.github.com>
2024-10-17 23:37:18 +0400

    Refactoring project #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles/style.css';
+
+/* Для удобства созданы переменные с символами */
+const SYMBOL_X = 'X';
+const SYMBOL_O = 'O';
 
 function App() {
+  const cells = [null, null, null, SYMBOL_X, SYMBOL_O, SYMBOL_X, null, null, null];
+
+  const currentStep = SYMBOL_O;
+  const getSymbol = (symbol) => {
+    if (symbol === SYMBOL_O) return "symbol symbol--o";
+    if (symbol === SYMBOL_X) return "symbol symbol--x";
+    return '';
+  }
+
+  const renderSymbol = (symbol) => <span className={`symbol ${getSymbol(symbol)}`}>{symbol}</span>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="game">
+        <div className="game-info">
+          Ход: {renderSymbol(currentStep)}
+        </div>
+        <div className="game-field">
+          {cells.map(symbol => {
+            return <button className="cell">{ symbol ? renderSymbol(symbol) : null }</button>
+          })}
+        </div>
+      </div>
+  )
 }
 
 export default App;
